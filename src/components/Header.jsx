8e6acab9165a37b73b104e0e8a858f1fb8ce122f
Matt Baxter <mbx33@hotmail.com>
2022-05-16 22:25:36 -0600

    changed link to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,71 +1,69 @@
-import styled from "styled-components";
-import { Link } from "react-scroll";
+import styled from 'styled-components';
+import { Link } from 'react-scroll';
 
 function Header() {
-  return (
-    <MainContainer>
-      <div className="logo">
-        <a href="/">
-          <h1>Matthew Baxter</h1>
-        </a>
-      </div>
-      <Menu>
-        <Link to="projects" spy={true} smooth={true} duration={500}>
-          <a href="/">Projects</a>
-        </Link>
-        <Link to="links" spy={true} smooth={true} duration={500}>
-          <a href="/">Socials</a>
-        </Link>
-        <a href="mailto:mbx33@hotmail.com?subject=Hello%20Matt!">Contact</a>
-        {/* <a href="/">Contact</a> */}
-        {/* <a href="/">About</a> */}
-      </Menu>
-    </MainContainer>
-  );
+	return (
+		<MainContainer>
+			<div className="logo">
+				<a href="/">
+					<h1>Matthew Baxter</h1>
+				</a>
+			</div>
+			<Menu>
+				<Link to="projects" spy={true} smooth={true} duration={500}>
+					<a href="/">Projects</a>
+				</Link>
+				<Link to="links" spy={true} smooth={true} duration={500}>
+					<a href="/">Socials</a>
+				</Link>
+				<a href="mailto:mbx33@hotmail.com?subject=Hello%20Matt!">Contact</a>
+			</Menu>
+		</MainContainer>
+	);
 }
 
 export default Header;
 
 const MainContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  height: 6vh;
-  padding: 20px;
+	display: flex;
+	justify-content: space-between;
+	height: 6vh;
+	padding: 20px;
 
-  .logo {
-    a {
-      font-size: 20px;
-      color: rgba(34, 34, 34, 0.692);
-    }
-    h1 {
-      cursor: default;
-    }
-  }
+	.logo {
+		a {
+			font-size: 20px;
+			color: rgba(34, 34, 34, 0.692);
+		}
+		h1 {
+			cursor: default;
+		}
+	}
 
-  a {
-    text-decoration: none;
-  }
+	a {
+		text-decoration: none;
+	}
 
-  @media (max-width: 768px) {
-    text-align: center;
-    justify-content: space-evenly;
-  }
+	@media (max-width: 768px) {
+		text-align: center;
+		justify-content: space-evenly;
+	}
 `;
 
 const Menu = styled.div`
-  align-items: center;
+	align-items: center;
 
-  a {
-    padding: 10px 20px;
-    font-weight: 550;
-    cursor: pointer;
-    letter-spacing: 1px;
-    font-size: 20px;
-    text-transform: uppercase;
-    color: rgba(34, 34, 34, 0.692);
-  }
+	a {
+		padding: 10px 20px;
+		font-weight: 550;
+		cursor: pointer;
+		letter-spacing: 1px;
+		font-size: 20px;
+		text-transform: uppercase;
+		color: rgba(34, 34, 34, 0.692);
+	}
 
-  @media (max-width: 768px) {
-    display: none;
-  }
+	@media (max-width: 768px) {
+		display: none;
+	}
 `;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,204 +1,200 @@
-import React from "react";
-import styled from "styled-components";
-import ReactPlayer from "react-player/lazy";
+import React from 'react';
+import styled from 'styled-components';
+import ReactPlayer from 'react-player/lazy';
 
 function Projects() {
-  return (
-    <Container id="projects">
-      <ProjectWrap>
-        <SideArticleContainer>
-          <div className="article-content">
-            <h2>Recent Projects</h2>
-            <br />
-            <p>
-              A few of my most recent projects I've worked on. More demos coming
-              soon.
-            </p>
-            <ol className="projects">
-              <li>
-                <h3>Project: Money Tracker</h3>
-                <p>
-                  A single page app that uses firebase to authenticate and store
-                  data entered by the users.
-                </p>
-                <a
-                  href="https://money-tracker-fc1f0.web.app/login"
-                  target="[_]blank"
-                >
-                  Check it out!
-                </a>
-              </li>
-            </ol>
-            <ul className="challenges">
-              <h3>{`< Frontend Challenges />`}</h3>
-              <li>
-                <p>Business Landing Page</p>
-                <a
-                  href="https://github.com/mbx33/landing-page-challenge"
-                  target="[_]blank"
-                >
-                  Github Repo
-                </a>
-              </li>
-            </ul>
-          </div>
-        </SideArticleContainer>
+	return (
+		<Container id="projects">
+			<ProjectWrap>
+				<SideArticleContainer>
+					<div className="article-content">
+						<h2>Recent Projects</h2>
+						<br />
+						<p>
+							A few of my most recent projects I've worked on. More demos coming soon.
+						</p>
+						<ol className="projects">
+							<li>
+								<h3>Project: Money Tracker</h3>
+								<p>
+									A single page app that uses firebase to authenticate and store data
+									entered by the users.
+								</p>
+								<a href="https://money-tracker-fc1f0.web.app/login" target="[_]blank">
+									Check it out!
+								</a>
+							</li>
+						</ol>
+						<ul className="challenges">
+							<h3>{`< Frontend Challenges />`}</h3>
+							<li>
+								<p>Business Landing Page</p>
+								<a
+									href="https://mbx33.github.io/landing-page-challenge/"
+									target="[_]blank"
+								>
+									Github Repo
+								</a>
+							</li>
+						</ul>
+					</div>
+				</SideArticleContainer>
 
-        <LinkContainer>
-          <LinkWrap>
-            <p>Sightseeing Tour sight..(no audio)</p>
-            <ReactPlayer
-              title="project1"
-              width="380px"
-              height="280px"
-              url="https://youtu.be/0HdMJELXs9c"
-              frameborder="0"
-              muted="true"
-              controls="true"
-              style={{ border: "2px solid white" }}
-            />
-          </LinkWrap>
-          <LinkWrap>
-            <p>Coming Soon..</p>
-            <ReactPlayer
-              title="project1"
-              width="380px"
-              height="280px"
-              url="#"
-              frameborder="0"
-              muted="true"
-              controls="true"
-              style={{ border: "2px solid white" }}
-            />
-          </LinkWrap>
-          <LinkWrap>
-            <p>Coming Soon..</p>
-            <ReactPlayer
-              title="project1"
-              width="380px"
-              height="280px"
-              url="#"
-              frameborder="0"
-              muted="true"
-              controls="true"
-              style={{ border: "2px solid white" }}
-            />
-          </LinkWrap>
-        </LinkContainer>
-      </ProjectWrap>
-    </Container>
-  );
+				<LinkContainer>
+					<LinkWrap>
+						<p>Sightseeing Tour sight..(no audio)</p>
+						<ReactPlayer
+							title="project1"
+							width="380px"
+							height="280px"
+							url="https://youtu.be/0HdMJELXs9c"
+							frameborder="0"
+							muted="true"
+							controls="true"
+							style={{ border: '2px solid white' }}
+						/>
+					</LinkWrap>
+					<LinkWrap>
+						<p>Coming Soon..</p>
+						<ReactPlayer
+							title="project1"
+							width="380px"
+							height="280px"
+							url="#"
+							frameborder="0"
+							muted="true"
+							controls="true"
+							style={{ border: '2px solid white' }}
+						/>
+					</LinkWrap>
+					<LinkWrap>
+						<p>Coming Soon..</p>
+						<ReactPlayer
+							title="project1"
+							width="380px"
+							height="280px"
+							url="#"
+							frameborder="0"
+							muted="true"
+							controls="true"
+							style={{ border: '2px solid white' }}
+						/>
+					</LinkWrap>
+				</LinkContainer>
+			</ProjectWrap>
+		</Container>
+	);
 }
 
 export default Projects;
 
 const Container = styled.div`
-  padding: 2em 0;
-  height: 100%;
-  width: 100%;
-  color: white;
+	padding: 2em 0;
+	height: 100%;
+	width: 100%;
+	color: white;
 `;
 
 const ProjectWrap = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  padding-bottom: 20px;
+	display: flex;
+	width: 100%;
+	height: 100%;
+	padding-bottom: 20px;
 
-  @media (max-width: 768px) {
-    flex-direction: column;
-    /* padding: 2rem; */
-  }
+	@media (max-width: 768px) {
+		flex-direction: column;
+		/* padding: 2rem; */
+	}
 
-  @media (max-width: 425px) {
-    padding: 0;
-  }
+	@media (max-width: 425px) {
+		padding: 0;
+	}
 `;
 
 const LinkContainer = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  flex-direction: column;
-  align-items: center;
-  width: 50%;
-  /* border: 2px solid red; */
+	display: flex;
+	justify-content: space-evenly;
+	flex-direction: column;
+	align-items: center;
+	width: 50%;
+	/* border: 2px solid red; */
 
-  @media (max-width: 400px) {
-    width: 100vw;
-    /* padding: 0 1rem; */
-  }
+	@media (max-width: 400px) {
+		width: 100vw;
+		/* padding: 0 1rem; */
+	}
 `;
 
 const LinkWrap = styled.div`
-  width: 50%;
-  height: 100%;
+	width: 50%;
+	height: 100%;
 
-  /* background-repeat: no-repeat; */
-  margin: 10px auto;
+	/* background-repeat: no-repeat; */
+	margin: 10px auto;
 
-  @media (max-width: 768px) {
-    width: 90%;
-  }
+	@media (max-width: 768px) {
+		width: 90%;
+	}
 `;
 
 const SideArticleContainer = styled.div`
-  top: 0;
-  width: 50%;
+	top: 0;
+	width: 50%;
 
-  h2 {
-    letter-spacing: 0.3rem;
-    text-align: center;
-  }
-  .article-content {
-    width: 65%;
-    margin: 2em auto;
+	h2 {
+		letter-spacing: 0.3rem;
+		text-align: center;
+	}
+	.article-content {
+		width: 65%;
+		margin: 2em auto;
 
-    a {
-      color: orange;
-      font-size: 1.5rem;
-    }
+		a {
+			color: orange;
+			font-size: 1.5rem;
+		}
 
-    p {
-      /* text-align: center; */
-      font-size: 18px;
-      line-height: 1.5;
-    }
+		p {
+			/* text-align: center; */
+			font-size: 18px;
+			line-height: 1.5;
+		}
 
-    .projects {
-      padding-top: 2rem;
+		.projects {
+			padding-top: 2rem;
 
-      li {
-        list-style-type: none;
-      }
-    }
-    .challenges {
-      padding-top: 6rem;
-      li {
-        padding: 1rem 0;
-        list-style-type: none;
-      }
-    }
-  }
+			li {
+				list-style-type: none;
+			}
+		}
+		.challenges {
+			padding-top: 6rem;
+			li {
+				padding: 1rem 0;
+				list-style-type: none;
+			}
+		}
+	}
 
-  @media (max-width: 768px) {
-    width: 100%;
-    padding: 1rem;
-    /* text-align: center; */
+	@media (max-width: 768px) {
+		width: 100%;
+		padding: 1rem;
+		/* text-align: center; */
 
-    .article-content {
-      padding: 1rem;
-      margin: 0 auto;
-      width: 80%;
+		.article-content {
+			padding: 1rem;
+			margin: 0 auto;
+			width: 80%;
 
-      p {
-        /* text-align: center; */
-      }
-    }
+			p {
+				/* text-align: center; */
+			}
+		}
 
-    .challenges {
-      h3 {
-        font-size: 1.1rem;
-      }
-    }
-  }
+		.challenges {
+			h3 {
+				font-size: 1.1rem;
+			}
+		}
+	}
 `;
