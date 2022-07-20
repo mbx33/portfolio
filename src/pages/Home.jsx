a59a57769b8765b616be22602bab12bd7fad968c
Matt Baxter <mbx33@hotmail.com>
2022-07-20 11:40:30 -0600

    fixed html tags and margins for them

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import Demos from '../components/Demos';
 import Projects from '../components/Projects';
-import Section from '../components/Section';
+import Hero from '../components/Hero';
 import Skills from '../components/Skills';
 // import SoftSkills from "../components/SoftSkills";
 
 function Home() {
 	return (
 		<Container>
-			<Section />
+			<Hero />
 			<Skills />
 			{/* <SoftSkills /> */}
 			<Projects />
