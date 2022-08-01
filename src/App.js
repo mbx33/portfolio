d9a45e2007768282dfe07535da11209f9ba65e7f
Matt Baxter <mbx33@hotmail.com>
2022-08-01 01:14:04 -0600

    finished styling contact form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import "./App.css";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // page imports
-import Home from "./pages/Home";
+import Home from './pages/Home';
 
 // component imports
-import Footer from "./components/Footer";
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Home />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
