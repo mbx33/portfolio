ce8a4135382a3b39eef13f10b217ee33ea1a6f3a
Matt Baxter <mbx33@hotmail.com>
2022-10-30 23:07:47 -0600

    changing the scroll btn border radius

--- src/components/LinkUp.jsx
+++ src/components/LinkUp.jsx
@@ -15,7 +15,7 @@ const Wrapper = styled.div`
 	.btn-up {
 		font-size: 4rem;
 		padding: 1rem;
-		border-radius: 50%;
+		border-radius: 10%;
 		color: #f4f4f4;
 
 		background-color: hsl(211, 100%, 60%, 0.8);
